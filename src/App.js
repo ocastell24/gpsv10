fe5b0aa47aa181fe1a62488eac838bf87dbe1fa4
ocastell24 <ocastell@hotmail.com>
2022-09-15 23:12:23 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
-import Amplify from 'aws-amplify';
-import awsconfig  from './aws-exports';
-import { AmplifySignOut, withAuthenticator  } from '@aws-amplify/ui-react-v1';
+//import logo from './logo.svg';
+import { I18n } from 'aws-amplify';
+//import './App.css';
+import '@aws-amplify/ui-react/styles.css';
+import { Amplify } from 'aws-amplify';
+//import awsconfig from './aws-exports';
+import { AmplifySignOut} from '@aws-amplify/ui-react-v1';
+import React from 'react'
+import { Divider, translations,withAuthenticator , Button, Heading, Flex, ButtonGroup } from '@aws-amplify/ui-react';
 
-Amplify.configure(awsconfig)
+import awsExports from "./aws-exports";
 
+Amplify.configure(awsExports);
+// Configurar a Lenguaje Espanol
+I18n.putVocabularies(translations);
+I18n.setLanguage('es');
 
-function App() {
+const styles = {
+  container: { width: 400, margin: '0 auto', display: 'flex', flexDirection: 'column', justifyContent: 'center', padding: 20 },
+  mainContainer: { paddingTop: '150px' },
+  button: { backgroundColor: 'black', color: 'white', outline: 'none', fontSize: 18, padding: '12px 0px' },
+  btnSign: { textAlign: 'right' }
+}
+
+function App({ signOut, user }) {
   return (
-    <div className="App">
-      <header className="App-header">
-      <AmplifySignOut />
-       <h2>Hola Valentina</h2> 
-      </header>
-    </div>
+    <>
+      <Flex
+        direction="row"
+        justifyContent="space-between"
+        alignItems="center"
+        alignContent="center"
+        wrap="wrap">
+        <Heading level={4}>Hola {user.username}</Heading>
+        <Button style={styles.btnSign} onClick={signOut}>Salir</Button>
+      </Flex>
+
+      <Flex direction="column">
+        <Divider
+          size="large"
+          orientation="horizontal"
+        />
+      </Flex>
+      {/* Add Todo JSX here  */
+        <div style={styles.mainContainer}>
+          <ButtonGroup
+            variation='primary'
+            size="large"
+            direction="column"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Button
+              onClick={() => alert('Implementar la pagina Flota.')}>
+              Flota</Button>
+            <Button
+              onClick={() => alert('Implementar la pagina Vehiculo.')}>
+              Vehiculo</Button>
+            <Button
+              onClick={() => alert('Implementar la pagina Reportes.')}
+            >Reportes</Button>
+          </ButtonGroup>
+        </div>
+      }
+    </>
   );
 }
 
