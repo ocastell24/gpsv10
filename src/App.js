e8e9341a41b20f2bb1c3b777ba23f8e1ec68e946
ocastell24 <ocastell@hotmail.com>
2022-09-18 11:36:17 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -5,24 +5,32 @@ import '@aws-amplify/ui-react/styles.css';
 import { Amplify } from 'aws-amplify';
 //import awsconfig from './aws-exports';
 import { AmplifySignOut } from '@aws-amplify/ui-react-v1';
-import React, { Component } from 'react'
+import React from 'react'
 import { Divider, translations, withAuthenticator, Button, Heading, Flex, ButtonGroup } from '@aws-amplify/ui-react';
-import awsExports from "./aws-exports";
 import Pantalla2 from './Componentes/Pantallas/Pantalla2';
-
+import awsExports from "./aws-exports";
 
 Amplify.configure(awsExports);
 // Configurar a Lenguaje Espanol
 I18n.putVocabularies(translations);
 I18n.setLanguage('es');
 
-
+/*const styles = {
+  container: { width: 400, margin: '0 auto', display: 'flex', flexDirection: 'column', justifyContent: 'center', padding: 20 },
+  mainContainer: { paddingTop: '150px' },
+  button: { backgroundColor: 'black', color: 'white', outline: 'none', fontSize: 18, padding: '12px 0px' },
+  btnSign: { textAlign: 'right' }
+}*/
 
 function App({ signOut, user }) {
   return (
-    <Pantalla2 user={ user }
-               signOut={ signOut } />
+    
+      <Pantalla2 user={user}
+        signOut={signOut} />
+    
   );
 }
 
-export default App;
+export default withAuthenticator(App);
+
+//export default App;
