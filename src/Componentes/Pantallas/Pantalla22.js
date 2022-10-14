27c2e8526dbb08dc4a3caf710116f52e48f2b743
ocastell24 <ocastell@hotmail.com>
2022-10-13 22:05:55 -0500

    initial commit

--- src/Componentes/Pantallas/Pantalla22.js
+++ src/Componentes/Pantallas/Pantalla22.js
@@ -1,7 +1,8 @@
-import { Loader, MapView, View, Flex, Heading, Geocoder, Text, Image } from '@aws-amplify/ui-react';
+import { Loader, MapView, View, Flex, Heading, Geocoder
+       , Text, Image } from '@aws-amplify/ui-react';
 import { Amplify } from 'aws-amplify';
 import '@aws-amplify/ui-react/styles.css';
-import { Marker, Popup } from 'react-map-gl';
+import { Marker, Popup } from 'react-map-gl/dist/es5';
 import awsExports from '../../aws-exports';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
