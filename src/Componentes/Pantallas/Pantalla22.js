a8add55824d804051f328c47a19691504e1a75d0
ocastell24 <ocastell@hotmail.com>
2022-10-23 12:27:13 -0500

    initial commit

--- src/Componentes/Pantallas/Pantalla22.js
+++ src/Componentes/Pantallas/Pantalla22.js
@@ -1,20 +1,49 @@
 import {
-  Loader, MapView, View, Flex, Heading, Geocoder
-  , Text, Image
+  Loader, MapView, View, Flex, Heading, Geocoder, Text, Image, Button, LocationSearch
 } from '@aws-amplify/ui-react';
 import { Amplify } from 'aws-amplify';
 import '@aws-amplify/ui-react/styles.css';
-import { Marker, Popup } from 'react-map-gl/dist/es5';
+//import { Marker, Popup } from 'react-map-gl/dist/es5';
+import { Marker, Popup } from 'react-map-gl';
 import awsExports from '../../aws-exports';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import '@aws-amplify/ui-react/styles.css';
+import maplibregl from 'maplibre-gl';
+//import AWS from 'aws-sdk';
+import React, { useEffect, useRef } from 'react';
+import "maplibre-gl/dist/maplibre-gl.css";
+import "@maplibre/maplibre-gl-geocoder/dist/maplibre-gl-geocoder.css";
+import "maplibre-gl-js-amplify/dist/public/amplify-geocoder.css";
+import { createMap } from "maplibre-gl-js-amplify";
+
 
 Amplify.configure(awsExports);
 
 function Pantalla22() {
-  return (
-    <MapView />
-  )
-}
+  const mapRef = useRef(null);
 
+  useEffect(() => {
+    let map: maplibregl.Map;
+    async function initializeMap() {
+      if (mapRef.current != null) {
+        map = await createMap({
+          container: mapRef.current,
+          center: [-122.431297, 37.773972],
+          zoom: 11,
+        });
+      }
+    }
+    initializeMap();
+
+    return function cleanup() {
+      if (map != null) map.remove();
+    };
+  }, []);
+
+
+  return (
+    <div ref={mapRef} id="map" />
+  );
+}
 export default Pantalla22;
