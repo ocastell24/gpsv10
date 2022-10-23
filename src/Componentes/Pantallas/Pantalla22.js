e94b8d6069f283fa006df6a7449bce0a08e93e75
ocastell24 <ocastell@hotmail.com>
2022-10-23 17:26:58 -0500

    changes for v2

--- src/Componentes/Pantallas/Pantalla22.js
+++ src/Componentes/Pantallas/Pantalla22.js
@@ -1,17 +1,17 @@
-import {
-  Loader, MapView, View, Flex, Heading, Geocoder, Text, Image, Button, LocationSearch
-} from '@aws-amplify/ui-react';
+//import {
+// Loader, MapView, View, Flex, Heading, Geocoder, Text, Image, Button, LocationSearch
+//} from '@aws-amplify/ui-react';
 import { Amplify } from 'aws-amplify';
 import '@aws-amplify/ui-react/styles.css';
 //import { Marker, Popup } from 'react-map-gl/dist/es5';
-import { Marker, Popup } from 'react-map-gl';
+//import { Marker, Popup } from 'react-map-gl';
 import awsExports from '../../aws-exports';
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
-import '@aws-amplify/ui-react/styles.css';
-import maplibregl from 'maplibre-gl';
+//import { useState } from 'react';
+////import { Link } from 'react-router-dom';
+//import '@aws-amplify/ui-react/styles.css';
+//import maplibregl from 'maplibre-gl';
 //import AWS from 'aws-sdk';
-import React, { useEffect, useRef } from 'react';
+//import React, { useEffect, useRef } from 'react';
 //import "maplibre-gl/dist/maplibre-gl.css";
 //import "@maplibre/maplibre-gl-geocoder/dist/maplibre-gl-geocoder.css";
 //import "maplibre-gl-js-amplify/dist/public/amplify-geocoder.css";
